3a19f477dc138dd9dc76c56fe75f521fc0367a5c
Neven Dimač <neven.dimac@gmail.com>
2025-03-07 10:00:15 +0100

    Added routing and codebehind

---
+++ BlazorWebAppDemo/Demo/PageDemo.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebAppDemo.Demo;
+[Route("/PageDemoFromCodeBehind")]
+//it will navigate to /pagedemo from PageDemo.razor.cs
+public partial class PageDemo
+{
+
+}
