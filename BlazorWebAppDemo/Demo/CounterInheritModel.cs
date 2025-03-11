990e11d7e05c67b79295da40fdf2dca371ef306b
Neven Dimač <neven.dimac@gmail.com>
2025-03-11 13:56:14 +0100

    Codebehind files

---
+++ BlazorWebAppDemo/Demo/CounterInheritModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebAppDemo.Demo
+{
+    public class CounterInheritModel : ComponentBase
+    {
+        protected int currentCount = 0;
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
