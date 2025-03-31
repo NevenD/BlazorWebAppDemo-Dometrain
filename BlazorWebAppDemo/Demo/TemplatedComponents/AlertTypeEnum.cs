1727bb3be9d6520956bee33b9aed94506bc8c680
Neven Dimač <neven.dimac@gmail.com>
2025-03-31 20:50:16 +0200

    Render fragment type with child content

---
+++ BlazorWebAppDemo/Demo/TemplatedComponents/AlertTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorWebAppDemo.Demo.TemplatedComponents
+{
+    public enum AlertTypeEnum
+    {
+
+        Primary,
+        Secondary,
+        Success,
+        Danger,
+        Warning,
+        Info,
+        Light,
+        Dark
+    }
+}
